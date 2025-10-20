d95fa8f50f03dd35b6a7fdbbdcb043f93358dcdb
Nikita Slobozhanin <73555899+Borsu4ok@users.noreply.github.com>
2025-10-20 13:50:12 +0300

    Add files via upload

---
+++ consapp1_2.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter an integer:");
+string? input = Console.ReadLine();
+if (int.TryParse(input, out int number))
+{
+    Console.WriteLine($"You wrote {number}");
+}
+else
+{
+    Console.WriteLine("This is not an integer");
+}
+
